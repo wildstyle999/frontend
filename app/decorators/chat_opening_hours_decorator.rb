d107e2fcde9a0b75870a5ec8da828b4ecb191eca
Andrew Garner <andrew@andrewgarner.com>
2014-08-13 17:46:14 +0100

    Add chat behavior

--- app/decorators/chat_opening_hours_decorator.rb
+++ app/decorators/chat_opening_hours_decorator.rb
@@ -1,2 +1,25 @@
 class ChatOpeningHoursDecorator < Draper::Decorator
+  delegate :now_open?
+
+  def description
+    if Feature.inactive?(:chat)
+      I18n.t('contact_panels.web_chat.coming_soon.description_html')
+    elsif now_open?
+      I18n.t('contact_panels.web_chat.available.description')
+    else
+      I18n.t('contact_panels.web_chat.offline.description')
+    end
+  end
+
+  def button_label
+    if Feature.inactive?(:chat) || now_open?
+      I18n.t('contact_panels.web_chat.unavailable.button')
+    else
+      I18n.t('contact_panels.web_chat.offline.button')
+    end
+  end
+
+  def status
+    now_open? ? 'open' : 'closed'
+  end
 end
