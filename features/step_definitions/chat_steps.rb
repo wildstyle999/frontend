f31566aa7b21642f09c39a4ad2910ebb53823cb9
Andrew Garner <andrew@andrewgarner.com>
2014-08-15 16:43:58 +0100

    WIP

--- features/step_definitions/chat_steps.rb
+++ features/step_definitions/chat_steps.rb
@@ -1,41 +1,51 @@
 Given(/^chat is online$/) do
-  pending
-end
+  Timecop.travel(Chronic.parse('Tuesday 2pm'))
 
-Given(/^chat is offline$/) do
-  pending
+  home_page.load(locale: :en)
 end
 
 Given(/^an advisor is available$/) do
-  pending
+  true
 end
 
 Given(/^all advisors are busy$/) do
   pending
 end
 
+Given(/^chat will be next online later today$/) do
+  Timecop.travel(Chronic.parse('Tuesday 5am'))
+
+  home_page.load(locale: :en)
+end
+
+Given(/^chat will be next online tomorrow$/) do
+  Timecop.travel(Chronic.parse('Tuesday 11pm'))
+
+  home_page.load(locale: :en)
+end
+
 When(/^I visit the website in Welsh$/) do
   home_page.load(locale: :cy)
 end
 
-When(/^chat will be next online later today$/) do
-  pending
-end
-
-When(/^chat will be next online tomorrow$/) do
-  pending
-end
-
 Then(/^I should be able to start a chat with an advisor$/) do
-  pending
+  expect(home_page.chat.button).
+    to have_content(I18n.t('contact_panels.web_chat.available.button'))
+
+  expect(home_page.chat.description).
+    to have_content(I18n.t('contact_panels.web_chat.available.description'))
 end
 
 Then(/^I should not be able to start a chat with an advisor$/) do
-  expect(home_page.chat.button).to be_disabled
+  expect(home_page.chat.button).
+    to have_content(I18n.t('contact_panels.web_chat.offline.button'))
+
+  expect(home_page.chat.description).
+    to have_content(I18n.t('contact_panels.web_chat.offline.description'))
 end
 
 Then(/^I should see a message informing me that I need JavaScript in order chat with an advisor$/) do
-  pending
+  expect(home_page.chat).to have_javascript_warning
 end
 
 Then(/^I should see a message informing me that chat is currently busy$/) do
@@ -43,11 +53,13 @@ Then(/^I should see a message informing me that chat is currently busy$/) do
 end
 
 Then(/^I should see a message informing me that chat will be online between today's opening hours$/) do
-  pending
+  expect(home_page.chat.description).
+    to have_content(I18n.t('contact_panels.web_chat.offline.description'))
 end
 
 Then(/^I should see a message informing me that chat will be online tomorrow with tomorrow's opening hours$/) do
-  pending
+  expect(home_page.chat.description).
+    to have_content(I18n.t('contact_panels.web_chat.offline.description'))
 end
 
 Then(/^I should see a message informing me that chat is only available in English$/) do
--- features/support/ui/sections/chat.rb
+++ features/support/ui/sections/chat.rb
@@ -2,8 +2,9 @@ require_relative '../section'
 
 module UI::Sections
   class Chat < UI::Section
-    element :description, '.t-chat-description'
     element :button, '.t-chat-button'
+    element :description, '.t-chat-description'
+    element :javascript_warning, '.t-chat-javascript'
     element :opening_times, '.t-chat-opening-times'
     element :welsh_warning, '.t-welsh-warning'
   end
