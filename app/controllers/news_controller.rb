506d85ba3e55765dadad941dc5f8106f2e9e4145
Douglas Roper <dougdroper@gmail.com>
2015-01-06 10:02:48 +0000

    Remove latest news from news article

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,13 +1,11 @@
 class NewsController < ApplicationController
   decorates_assigned :news_article, with: NewsArticleDecorator
-  decorates_assigned :latest_news, with: NewsDecorator
 
   def show
     @news_article = Core::NewsArticleReader.new(params[:id]).call do
       not_found
     end
 
-    @latest_news = Core::NewsReader.new(page_number: 1, limit: 6).call
     @breadcrumbs = BreadcrumbTrail.build(@news_article, category_tree)
   end
 end
